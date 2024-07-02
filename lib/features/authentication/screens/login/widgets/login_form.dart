16c89d4cc710784356a248eb6a269e2d229b2c0f
Nirajan Bohara <boharanirajan430@gmail.com>
2024-07-02 22:07:04 +0545

    navigation menu made

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:purchasepoint/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:purchasepoint/features/authentication/screens/signup/signup.dart';
+import 'package:purchasepoint/navigation_menu.dart';
 import 'package:purchasepoint/utils/constants/sizes.dart';
 import 'package:purchasepoint/utils/constants/text_strings.dart';
 
@@ -62,7 +63,7 @@ class TLoginForm extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () => Get.to(() => const NavigationMenu()),
                 child: const Text(TTexts.signIn),
               ),
             ),
